bf1ab3a74b6721f0b9aa7e342f8d47b461560fa9
rpartha <tilluboy@gmail.com>
2018-02-25 01:33:28 -0500

    bug fixes and clean up

--- Driver.java
+++ Driver.java
@@ -1,29 +1,66 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Driver {
 	public static void main(String[] args){
 
-		long beg = Ssytem.nanoTime();
 
-		Grid grid = new Grid();
-		grid.printGrid(null);
-		ArrayList<Node> al = RepeatedAStar.executePath(true, grid, true);
 
-		long end =System.nanoTime();
+		Scanner scanner = new Scanner(System.in); 
+		System.out.print("READ (1) or WRITE (2): ");
 
-		if(al == null){
-			System.out.println("This puzzle is impossible!");
-			return;
+		int option = Integer.parseInt(scanner.nextLine());
+
+		if(option == 1){
+			System.out.print("Enter file name you wish to READ: ");
+			String fname = scanner.nextLine();
+
+			long beg = System.nanoTime();
+
+			Grid grid = new Grid(fname);
+			grid.printGrid(null);
+
+			ArrayList<Node> al = RepeatedAStar.executePath(true, grid, true);
+
+			long end =System.nanoTime();
+
+			if(al == null){
+				System.out.println("This puzzle is impossible!");
+				scanner.close();
+				return;
+			} 
+			grid.printGrid(al);
+			//grid.printGrid(null);
+
+			long total = end - beg;
+
+			System.out.println(total);
+			scanner.close();
+		} else if(option == 2){
+			System.out.print("Enter file name you wish to WRITE to: ");
+			String fname = scanner.nextLine();
+
+			//long beg = System.nanoTime();
+
+			Grid grid = new Grid();
+			grid.printGrid(null);
+
+			ArrayList<Node> al = RepeatedAStar.executePath(true, grid, true);
+
+			//long end =System.nanoTime();
+
+			if(al == null){
+				System.out.println("This puzzle is impossible!");
+				scanner.close();
+				return;
+			} else{
+				grid.writeToFile(grid.array, fname);
+				scanner.close();
+			}
+		} else{
+			System.out.println("Invalid option!");
+			scanner.close();
+			System.exit(0);
 		}
-		/*for(Node node : al){
-			System.out.println(node.getX() + " , " + node.getY());
-		}*/
-		grid.printGrid(al);
-
-
-		long total = end - beg;
-
-		System.out.println(total);
-
 	}
 }
--- Grid.java
+++ Grid.java
@@ -1,6 +1,15 @@
 import java.util.Random;
 import java.util.ArrayList;
 
+import java.util.Random;
+import java.util.Scanner;
+import java.io.File;
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.io.IOException;
+
 
 public class Grid {
 	Random random = new Random();
@@ -27,6 +36,69 @@ public class Grid {
 			}
 		}
 	}
+
+	public Grid(String fname){
+		array = new Node[101][101];
+		try{
+			String[][] map = new String[101][101];
+			//File file = new File();
+            BufferedReader br = new BufferedReader(new FileReader("C:/Users/tillu/Documents/Trajectory_Replanning/" + fname));
+    
+            for (int i = 0; i < 101; i++) {
+              String line = br.readLine();
+          
+              for (int j = 0; j < 101; j++) {
+                map[j][i] = String.valueOf(line.charAt(j));
+              }
+            }
+			
+			for(int m =0; m < array.length; m++){
+				for(int n = 0; n < array[m].length; n++){
+					if(map[n][m].equals("B")){
+						array[n][m] =new Node(n,m,true);
+						
+					} else{
+						array[n][m] = new Node(n,m,false);
+					}
+				}
+			}
+
+            br.close();
+			
+		} catch(IOException e){
+			e.printStackTrace();
+        }
+	}
+
+	public void writeToFile(Node[][] terrain, String fname){
+		try{
+			File file = new File("C:/Users/tillu/Documents/Trajectory_Replanning/" + fname);
+
+			if(!file.exists()){
+				file.createNewFile();
+			}
+
+			BufferedWriter bw = new BufferedWriter(new FileWriter(fname));
+
+			//write contents to file
+			for(int i = 0; i < array.length; i++){
+				for(int j = 0; j < array[i].length; j++){
+					if(array[j][i].getBlocked()){
+						bw.write("B"); 
+					} else{
+						bw.write("U");
+					}
+				}
+
+				bw.newLine();
+			}
+
+			bw.close();
+
+		} catch(IOException e){
+			e.printStackTrace();
+		}
+	}
 	
 	public void printGrid(ArrayList<Node> path){
 		boolean exists = false;
@@ -50,7 +122,7 @@ public class Grid {
 				}
 			}
 			int index = 0;
-			int count = 0;
+			//int count = 0;
 			for(index = 0; index < path.size(); index++){
 				System.out.println();
 				for(int j = 0; j < 101; j++){
